aa15807b9c3c6776947ae67b98e63e35b5266251
SimonQuartier <45261531+SimonQuartier@users.noreply.github.com>
2019-05-12 12:02:20 +0200

    Add files via upload
    Name array for autocomplete added

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,21 @@ var db   = require(path.resolve('./', 'db.js'));
 // ----------------------------------------------------------------------------------------
 // Init
 
+let uninited = {
+	'name' : "foods",
+	'children' : []
+};
 let FOOD_GROUPS_JSON = {
 	'name' : "foods",
 	'children' : []
 };
+let NAMES = [];
 
 function init() {
+	
+	// --------------------------------------------
+	// Food Group
+
 	let get_foodgroup_sql = "select food_group from foods group by food_group"; 
 	db.query(get_foodgroup_sql, (err, result, fields) => {
 		if (err) {console.log(err)}
@@ -17,6 +26,10 @@ function init() {
 		result.forEach(res => {
 			const food_group = res.food_group;
 			let subgroups = [];
+			NAMES.push(food_group);
+
+			// --------------------------------------------
+			// Food Subgroup
 
 			let get_subgroup_of_foodgroup_sql = "select distinct food_subgroup from foods where food_group='"+food_group+"'"; 
 			db.query(get_subgroup_of_foodgroup_sql, (err, result, fields) => {
@@ -25,12 +38,18 @@ function init() {
 				result.forEach(res => {
 					const food_subgroup = res.food_subgroup;
 					let foods = [];
+					NAMES.push(food_subgroup);
+
+ 					// --------------------------------------------
+					// Food
 
 					let get_food_of_subgroup_sql = 'select distinct id,name,name_scientific,description from foods where food_subgroup="' + food_subgroup + '"';
 					db.query(get_food_of_subgroup_sql, (err, result, fields) => {
 						if (err) {console.log(err)};
 
+
 						foods = Array.from(result, res => {
+							NAMES.push(res.name);
 							return {
 								name: res.name,
 								id: res.id,
@@ -53,27 +72,17 @@ init();
 // ----------------------------------------------------------------------------------------
 // API
 
-module.exports = function (app) {
+module.exports = app => {
 
 	app.get('/getFoodGroups', (req, res) => {
-		const food_name = req.body.food_name;
 		let result = FOOD_GROUPS_JSON;
-    	if (result === []) { res.status(500).send({status: "err, init is not finished/run"}); }
-    	else { res.json({status: result}); }
+    	if (result === uninited) { res.status(500).send({status: "err, init is not finished/run"}); }
+    	else { res.json({json: FOOD_GROUPS_JSON, names: NAMES}); }
   	});
-
-	app.get('/getFood', (req, res) => {
-		const food_name = req.body.food_name;
-		let sql = 'select * from foods where name='+foodname+'';
-		db.query(sql, (err, result) => {
-    		if (err) { res.status(500).send({status: err}); }
-    		else { res.json({status: result[0]}); }
-  		});
-	});
 
 	// ---------------------------------------------------------------------------------
 	// Request Pages
-	app.get('/home', function(req, res) {
+	app.get('/home', (req, res) => {
 		res.sendFile('homepage.html', {root: path.join(__dirname, '../views/')});
 	});
 };
