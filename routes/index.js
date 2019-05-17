5b3dd05900dfa883af1bb9d8555571fcb33e7762
SimonQuartier <45261531+SimonQuartier@users.noreply.github.com>
2019-05-17 14:17:03 +0200

    Add files via upload
    request for compounds basics
    setting mime type for for the separation of the homepage css, html and js

--- routes/index.js
+++ routes/index.js
@@ -10,13 +10,15 @@ let FOOD_GROUPS_JSON = {
 };
 let NAMES = [];
 
+let init_done = false;
+
 const foodgroup_sql = "select food_group from foods group by food_group";
 const food_subgroup_sql = food_group => "select distinct food_subgroup from foods where food_group='"+food_group+"'" ;
 const food_sql = food_subgroup => 'select distinct id,name,name_scientific,description from foods where food_subgroup="'+food_subgroup+'"';
 
 const foodgroup_grade_sql = 'select food_group,min(grade),max(grade) from tbl_food_dishes group by food_group';
 const food_subgroup_grade_sql = food_group => 'select food_subgroup,min(grade),max(grade) from tbl_food_dishes group by food_group,food_subgroup having food_group="'+food_group+'"';
-const food_grade_sql = food_subgroup => 'select food_id,food_name,min(grade),max(grade) from tbl_food_dishes group by food_subgroup,food_name having food_subgroup="'+food_subgroup+'"';
+const food_grade_sql = food_subgroup => 'select food_id,food_name,min(grade),max(grade) from tbl_food_dishes group by food_subgroup,food_name,food_id having food_subgroup="'+food_subgroup+'"';
 
 function init() {
 	
@@ -29,54 +31,90 @@ function init() {
 
 		db.query(foodgroup_grade_sql, (err, food_groups_grades, fields) => {
 			if (err) {console.log(err)};
+
 			food_groups_grades.forEach(res => {
 				const food_group = res.food_group;
 				min_grades[food_group] = res['min(grade)'];
 				max_grades[food_group] = res['max(grade)'];
 			});
 
-		food_groups.forEach(res => {
-			const food_group = res.food_group;
-			NAMES.push(food_group);
-			let subgroups = [];
-
-			// ------------ Food Subgroup --------------------------------
-
-			db.query(food_subgroup_sql(food_group), (err, result, fields) => {
-				if (err) {console.log(err)};
-				let min_grades_subgroups = {};
-				let max_grades_subgroups = {};
-
-				db.query(food_subgroup_grade_sql(food_group), (err, result, fields) => {
-					if (err) {console.log(err)};
-					result.forEach(res => {
-						const food_subgroup = res.food_subgroup;
-						min_grades_subgroups[food_subgroup] = res['min(grade)'];
-						max_grades_subgroups[food_subgroup] = res['max(grade)'];
-					});
-				});
-
-				result.forEach(res => {
-					const food_subgroup = res.food_subgroup;
-					NAMES.push(food_subgroup);
-					let foods = [];					
-
- 					// ------------ Food --------------------------------
-
-					db.query(food_sql(food_subgroup), (err, result, fields) => {
-						if (err) {console.log(err)};
-						let min_grades_food = {};
-						let max_grades_food = {};
+			food_groups.forEach(res => {
+				const food_group = res.food_group;
+				NAMES.push(food_group);
+				let subgroups = [];
 	
-						db.query(food_grade_sql(food_subgroup), (err, result, fields) => {
-							if (err) {console.log(err)};
-							console.log(result)
-					//		result.forEach(res => {
-					//			const food_name = res.food_name;
-					//			min_grades_food[food_name] = res['min(grade)'];
-					//			max_grades_food[food_name] = res['max(grade)'];
-						//	});
+				// ------------ Food Subgroup --------------------------------
+	
+				db.query(food_subgroup_sql(food_group), (err, food_subgroups, fields) => {
+					if (err) {console.log(err)};
+					let min_grades_subgroups = {};
+					let max_grades_subgroups = {};
+	
+					db.query(food_subgroup_grade_sql(food_group), (err, food_subgroups_grades, fields) => {
+						if (err) {console.log(err)};
+
+						food_subgroups_grades.forEach(res => {
+							const food_subgroup = res.food_subgroup;
+							min_grades_subgroups[food_subgroup] = res['min(grade)'];
+							max_grades_subgroups[food_subgroup] = res['max(grade)'];
 						});
+					
+						food_subgroups.forEach(res => {
+							const food_subgroup = res.food_subgroup;
+							NAMES.push(food_subgroup);
+							let foods = [];					
+		
+ 							// ------------ Food --------------------------------
+		
+							db.query(food_sql(food_subgroup), (err, result, fields) => {
+								if (err) {console.log(err)};
+								let min_grades_food = {};
+								let max_grades_food = {};
+			
+								db.query(food_grade_sql(food_subgroup), (err, food_grades, fields) => {
+									if (err) {console.log(err)};
+
+									food_grades.forEach(res => {
+										const food_name = res.food_name;
+										min_grades_food[food_name] = res['min(grade)'];
+										max_grades_food[food_name] = res['max(grade)'];
+									});
+									foods = Array.from(result, res => {
+										NAMES.push(res.name);
+										return {
+											name: res.name,
+											id: res.id,
+											name_scientific: res.name_scientific,
+											description: res.description,
+											min_grade : min_grades_food[res.name],
+											max_grade : max_grades_food[res.name], 
+											size: 1
+										}
+									});
+									const food_subgroup_info = {
+										name : food_subgroup, 
+										min_grade : min_grades_subgroups[food_subgroup], 
+										max_grade : max_grades_subgroups[food_subgroup], 
+										children : foods
+									}
+									subgroups.push(food_subgroup_info);
+								});
+							});		
+						});
+					});
+					const food_group_info = {
+						name : food_group, 
+						min_grade : min_grades[food_group], 
+						max_grade : max_grades[food_group], 
+						children : subgroups
+					}
+					FOOD_GROUPS_JSON.children.push(food_group_info);					
+				});
+				init_done = true;
+			});
+		});
+  	});
+}
 	
 						/*
 						result.forEach(res => {
@@ -103,44 +141,7 @@ function init() {
 
 						});	
 						*/
-						
-						foods = Array.from(result, res => {
-							NAMES.push(res.name);
-						//	console.log(res.min_grade)
-							//let get_grades_of_food_sql = 'select grade ';
-							// TODO
-							return {
-								name: res.name,
-								id: res.id,
-								name_scientific: res.name_scientific,
-								description: res.description,
-								min_grade : 0,
-								max_grade : 5, 
-								size: 1
-							}
-							
-						});
-						const food_subgroup_info = {
-							name : food_subgroup, 
-							min_grade : min_grades_subgroups[food_subgroup], 
-							max_grade : max_grades_subgroups[food_subgroup], 
-							children : foods
-						}
-						subgroups.push(food_subgroup_info);
-					});
-				});			
-			});
-			const food_group_info = {
-				name : food_group, 
-				min_grade : min_grades[food_group], 
-				max_grade : max_grades[food_group], 
-				children : subgroups
-			}
-			FOOD_GROUPS_JSON.children.push(food_group_info);		
-		});
-		});
-  	});
-}
+					
 init();
 
 // ----------------------------------------------------------------------------------------
@@ -149,13 +150,37 @@ init();
 module.exports = app => {
 
 	app.get('/getFoodGroups', (req, res) => {
+		while (!init_done) {};
 		let result = FOOD_GROUPS_JSON;
    		res.json({json: FOOD_GROUPS_JSON, names: NAMES});
+  	});
+
+  	app.get('/getFoodByCompounds', (req, res) => {
+  		let compounds = req.body.compounds;
+		let result = []; f
+		let names = []; 
+
+		let sql;
+		db.query(food_sql(food_subgroup), (err, result, fields) => {
+			return
+
+			res.json({json: result, names: names});
+		});
   	});
 
 	// ---------------------------------------------------------------------------------
 	// Request Pages
 	app.get('/home', (req, res) => {
 		res.sendFile('homepage.html', {root: path.join(__dirname, '../views/')});
+	});
+
+	app.get('/style.css', (req, res) => {
+		res.type('text/css');
+		res.sendFile('style.css', {root: path.join(__dirname, '../views/')});
+	});
+
+	app.get('/script.js', (req, res) => {
+		res.type('text/javascript');
+		res.sendFile('script.js', {root: path.join(__dirname, '../views/')});
 	});
 };
